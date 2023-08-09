04850e6851aff131d0ab64d01d6459b1f76d9613
Jazmine Jose <jazzi.lyn217@yahoo.com>
2023-08-09 15:50:40 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let log = require('./logger.js');
+
+
+log('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+
+// sayHello('Jazmine');
+// console.log(window); (global object) //will not run because its not an object in node
+
+
+
+
+
